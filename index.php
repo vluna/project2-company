aa1c6c7932388bf57f8675bdeddd8541c24e302e
vluna <vluna.0293@gmail.com>
2016-06-09 13:10:01 -0700

    Changed index.html to .php + added includes folder containing the footer and navigation files + changed main.js to script.js + added grid.css and normalize.css + added sitemap and the axure file + added sitemap to styleguide navigation and citation

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	$title = "Home"; 
+	$description = "Home page for Vanocouver Meets a fictional company for IAT339 Project 2";
+	include ("includes/header.html");
+?>
+<h1> Home </h1>
+<?php
+	include ("includes/footer.html");
+?>

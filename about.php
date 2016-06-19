2037f9efcabced7d3c1e674564b26c024577d4dd
vluna <vluna.0293@gmail.com>
2016-06-19 16:34:15 -0700

    Added footer information

--- about.php
+++ about.php
@@ -24,10 +24,10 @@
 	<div id="about-values">
 		<h3>Values</h3>
 		<p>Integrity - From the hogs we buy to our manufacturing process, each step of the way, we operate with integrity. We are proud of every little thing we do.
-		Our people - We know that the key to success is having the right people in place to do the job. We simply couldn’t do it without the incredibly dedicated team at Sakura Farms.</p>
-		<p> Quality - While life is about compromise, when it comes to quality, at Sakura Farms we follow and abide by our strict guidelines! Staying true to our commitment to quality is one of our core mandates.</p>
-		<p>Innovation - While other manufacturers might be satisfied with sticking to the status quo, not us! At Sakura Farms, we’re not afraid of trying something new to find the best possible way of doing things.</p>
-		<p> Customers - We consider each and every customer a part of the Sakura Farms family - you are the lifeblood of our business and we strive each and every day to bring you the best of the best.</p>
+		Our people - We know that the key to success is having the right people in place to do the job. We simply couldn’t do it without the incredibly dedicated team at Vancouver Meats.</p>
+		<p> Quality - While life is about compromise, when it comes to quality, at Vancouver Meats we follow and abide by our strict guidelines! Staying true to our commitment to quality is one of our core mandates.</p>
+		<p>Innovation - While other manufacturers might be satisfied with sticking to the status quo, not us! At Vancouver Meats, we’re not afraid of trying something new to find the best possible way of doing things.</p>
+		<p> Customers - We consider each and every customer a part of the Vancouver Meats family - you are the lifeblood of our business and we strive each and every day to bring you the best of the best.</p>
 	</div>
 	<div id="about-producer">
 		<h3>Producer</h3>
@@ -39,32 +39,32 @@
 	</div>
 	<div id="about-facilities">
 		<h3>Facilities</h3>
-		<p>All processing, storage and distribution of Sakura Farms products are performed in federally inspected and HACCP accredited facilities. This ensures the highest levels of quality and food safety. In addition to our stringent Canadian standards, we are also British Retail Consortium (BRC) certified. But the buck doesn’t stop there...</p>
+		<p>All processing, storage and distribution of Vancouver Meats products are performed in federally inspected and HACCP accredited facilities. This ensures the highest levels of quality and food safety. In addition to our stringent Canadian standards, we are also British Retail Consortium (BRC) certified. But the buck doesn’t stop there...</p>
 
-		<p>We have set up strict protocols that are over and above any industry standard, all closely monitored by our Quality Assurance Department. At Sakura Farms, we live and breathe rigorous manufacturing practices. That’s just how we roll!</p>
+		<p>We have set up strict protocols that are over and above any industry standard, all closely monitored by our Quality Assurance Department. At Vancouver Meats, we live and breathe rigorous manufacturing practices. That’s just how we roll!</p>
 
-		<p>We use state of the art equipment and processes to deliver the quality you have come to rely on. From exact weight slicing technology to built-in, intermittent on-line sanitizers; fixed weight grinding technology to advanced carcass chilling systems - it’s all the best of the best! Our top-notch refrigeration systems keep our products fresh and safe. This commitment to new technology echoes one of our core values - use innovation to find a better way to do things. At Sakura Farms, we’re definitely not about following the crowd.</p>
+		<p>We use state of the art equipment and processes to deliver the quality you have come to rely on. From exact weight slicing technology to built-in, intermittent on-line sanitizers; fixed weight grinding technology to advanced carcass chilling systems - it’s all the best of the best! Our top-notch refrigeration systems keep our products fresh and safe. This commitment to new technology echoes one of our core values - use innovation to find a better way to do things. At Vancouver Meats, we’re definitely not about following the crowd.</p>
 
-		<p>What this all means for you is that anytime you purchase a Sakura Farms Premium meat product, you’re getting a quality cut of meat each and every time. Enjoy the best of food safety paired with a great shelf life.</p>
+		<p>What this all means for you is that anytime you purchase a Vancouver Meats Premium meat product, you’re getting a quality cut of meat each and every time. Enjoy the best of food safety paired with a great shelf life.</p>
 	</div>
 	<div id="about-pork">
 		<h3>Pork Attributes</h3>
 		<p>Our pigs are vegetable grain fed. No animal bone meal is fed to our pigs. Ever. Our pigs happily nosh on barley, peas, wheat, soy, minerals, and vitamins.</p>
 		<p>Barley is one of the main components of our feed rations. This results in fat that is whiter and firmer than what other producers, who use corn as the staple of their pigs’ diet, can offer. The result is a product with a richer texture and taste.</p>
-		<p>Our pigs are raised without the use of Dried Distillers Grains with Solubles (DDGS). DDGS is a cheaper feed additive prevalent in the meat industry. Sakura Farms producers only raise their pigs with premium feeds and without the use of byproducts such as DDGS (a byproduct of ethanol production).</p>
+		<p>Our pigs are raised without the use of Dried Distillers Grains with Solubles (DDGS). DDGS is a cheaper feed additive prevalent in the meat industry. Vancouver Meats producers only raise their pigs with premium feeds and without the use of byproducts such as DDGS (a byproduct of ethanol production).</p>
 		<p>Our pigs are also raised without the use of Ractopamine, a beta antagonist drug commonly used as an artificial feed additive to promote leanness in pigs and cattle. We grow our pigs in the traditional way. Period.</p>
 		<p>Our producers use antibiotics for therapeutic use only under the strict requirements of the Canadian Quality Assurance (CQA). They also use a long withdrawal period that goes above and beyond the guidelines set by Health Canada. This ensures that are no regulatory traces of antibiotics found in any of our products.</p>
-		<p>At Sakura Farms, we make absolutely no compromises when it comes to the care and diet of our hogs. We believe that healthy pigs make for tasty and nutritious pork!</p>
+		<p>At Vancouver Meats, we make absolutely no compromises when it comes to the care and diet of our hogs. We believe that healthy pigs make for tasty and nutritious pork!</p>
 	</div>
 	<div id="about-beef">
 		<h3>Beef Attributes</h3>
-		<p>Sakura Farms Organic Cattles are raised without the use of:</p>
+		<p>Vancouver Meats Organic Cattles are raised without the use of:</p>
 		<ul>
 			<li>Added Hormones</li>
 			<li>Antibiotics</li>
 			<li>Animal By-products</li>
 		</ul>
-		<p>At Sakura Farms, we make absolutely no compromises when it comes to the care and diet of our hogs and cattles. We believe that healthy pigs and cattle make for tasty and nutritious dishes!</p>
+		<p>At Vancouver Meats, we make absolutely no compromises when it comes to the care and diet of our hogs and cattles. We believe that healthy pigs and cattle make for tasty and nutritious dishes!</p>
 
 		<p>The supplier for our organic beef products is from Canadian Premium Meats(CPM). The CPM has the following certification and audits:</p>
 		<ul>
--- contact.php
+++ contact.php
@@ -27,15 +27,13 @@
                 <h3>INFORMATION</h3>
                 <p class="contact-information-description"></p>
 
-                <h4><img src="img/icon/clock.png" width="19" height="19"><span class="contact-icon-title">HOURS OF OPERATION</span></h4><p></p>
+                <p><img src="img/icon/clock.png" width="19" height="19"><span class="contact-icon-title">HOURS OF OPERATION</span></p><p></p>
 
-                <h4><img src="img/icon/phone.png" width="15" height="17"><span class="contact-icon-title">PHONE</span></h4><p></p>
+                <p><img src="img/icon/phone.png" width="15" height="17"><span class="contact-icon-title">PHONE</span></p><p></p>
 
-                <h4><img src="img/icon/email.png" width="20" height="13"><span class="contact-icon-title-email">EMAIL</span></h4><p></p>
-                
-                <h4><img src="img/icon/facebook-letter.png" width="12" height="20"><span class="contact-icon-title">FACEBOOK</span></h4><p></p>
+                <p><img src="img/icon/email.png" width="20" height="13"><span class="contact-icon-title-email">EMAIL</span></p><p></p>
 
-                <h4><img src="img/icon/location.png" width="15" height="20"><span class="contact-icon-title">ADDRESS</span></h4><p class="contact-information-description"></p>
+                <p><img src="img/icon/location.png" width="15" height="20"><span class="contact-icon-title">ADDRESS</span></p><p class="contact-information-description"></p>
             </div>
         </div>
     </div>
